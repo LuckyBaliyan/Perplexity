df97b0357e02403fbe7b6589a66d12329c046f87
Lucky Baliyan <baliyanlucky85@gmail.com>
2026-08-02 00:45:20 +0530

    fixing still the CORS

--- backend/src/app.js
+++ backend/src/app.js
@@ -27,13 +27,7 @@ const allowedOrigins = [
 ];
 
 app.use(cors({
-      origin(origin, callback) {
-            if (!origin || allowedOrigins.includes(origin)) {
-                  callback(null, true);
-            } else {
-                  callback(new Error("Not allowed by CORS"));
-            }
-      },
+      origin: allowedOrigins,
       credentials: true,
       methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
 }));
